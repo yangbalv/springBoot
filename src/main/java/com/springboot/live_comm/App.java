1d4d82492b3055f26734c2dfa67f0844683a45ae
张天一 <1539626240@qq.com>
2022-11-09 17:05:08 +0800

    SpringBoot-整合servlet,filter,listener,首先新建类分别继承HttpServlet，实现Filter，实现ServletRequestListener，然后分别加上@WebServlet，@WebFilter，@WebListener注解，最后在启动类上加上@ServletComponentScan

--- src/main/java/com/springboot/live_comm/App.java
+++ src/main/java/com/springboot/live_comm/App.java
@@ -4,8 +4,10 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class App {
     public static void main(String[] args) {
 //        SpringApplicationBuilder builder = new SpringApplicationBuilder();
---
+++ src/main/java/com/springboot/live_comm/configs/listener/MyListener.java
@@ -0,0 +1,18 @@
+package com.springboot.live_comm.configs.listener;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class MyListener implements ServletRequestListener {
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        System.out.println(this.getClass().getName() + ">>> requestDestroyed");
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        System.out.println(this.getClass().getName() + ">>> requestInitialized");
+    }
+}
