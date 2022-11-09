0a1e81a7dcfe4077b741e6d790115be64ae53c4a
张天一 <1539626240@qq.com>
2022-11-09 18:08:08 +0800

    SpringBoot-自动化配置类，去除特定的配置类,可以在项目的启动类中使用EnableAutoConfiguration来去除，也可以在application文件中添加spring，autoconfigure，exclude来配置。都配置不会出现异常，且这些自动化配置本身是有一定属性的，去除之后则没有属性了

--- src/main/java/com/springboot/live_comm/App.java
+++ src/main/java/com/springboot/live_comm/App.java
@@ -2,11 +2,14 @@ package com.springboot.live_comm;
 
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @ServletComponentScan
 public class App {
     public static void main(String[] args) {
