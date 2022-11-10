906d204375131d722c8190414a8cb78f4d6a2786
张天一 <1539626240@qq.com>
2022-11-10 17:13:34 +0800

    SpringBoot-数据库初始化以及建表语句

---
+++ 数据库文件/mysql_chapter05.sql
@@ -0,0 +1,14 @@
+-- mysql 5.0
+CREATE DATABASE `chapter05` DEFAULT CHARACTER SET utf8;
+USE `chapter05`;
+create table book
+(
+    id                   int(11) not null auto_increment,
+    name                 varchar(128) not null,
+    author               varchar(64) not null,
+    primary key (id)
+)
+    ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+
+INSERT INTO `book` (`id`,`name`,`author`) VALUES(1,'三国演义','罗贯中'),(2,'水浒传','施耐庵');
