c86bfb9eab4e9a8ec2c307c7926ad939762d2f84
张天一 <1539626240@qq.com>
2022-11-11 10:41:10 +0800

    SpringBoot-整合Mybatis-首先添加spring-boot-starter-web，mybatis-spring-boot-starter，druid,mysql-connector-java依赖，然后在任意目录添加Mapper接口，接口编辑好之后就可以在接口上添加@Mapper指明该类是一个Mapper接口或者在任意的配置类上加上@MapperScan（“接口存放地址”），表明该包下的任意接口都是Mapper,再创建Mapper.xml文件,此时这个mapper文件需要放在resources下相同的目录下，在pom中使用resource和includes重新指明资源文件位置，加载java路径下的xml文件，则mapper.xml文件则可直接放在mapper接口存放的位置。

--- src/main/java/com/springboot/live_comm/services/BookService.java
+++ src/main/java/com/springboot/live_comm/services/BookService.java
@@ -1,33 +1,60 @@
 package com.springboot.live_comm.services;
 
-import com.springboot.live_comm.dao.BookDao;
+//import com.springboot.live_comm.dao.BookDao;
 import com.springboot.live_comm.entity.Book;
+import com.springboot.live_comm.mappers.BookMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class BookService {
+//    JdbcTemplate配置数据库
+
+    //    @Autowired
+//    BookDao bookDao;
+//
+//    public int addBook(Book book) {
+//        return bookDao.addBook(book);
+//    }
+//
+//    public int updateBook(Book book) {
+//        return bookDao.updateBook(book);
+//    }
+//
+//    public int deleteBookById(Integer id) {
+//        return bookDao.deleteBookById(id);
+//    }
+//
+//    public Book getBookById(Integer id) {
+//        return bookDao.getBookById(id);
+//    }
+//
+//    public List<Book> getAllBooks() {
+//        return bookDao.getAllBooks();
+//    }
     @Autowired
-    BookDao bookDao;
+    BookMapper bookMapper;
 
     public int addBook(Book book) {
-        return bookDao.addBook(book);
+        return bookMapper.addBook(book);
     }
 
     public int updateBook(Book book) {
-        return bookDao.updateBook(book);
+        return bookMapper.updateBookById(book);
     }
 
     public int deleteBookById(Integer id) {
-        return bookDao.deleteBookById(id);
+        return bookMapper.deleteBookById(id);
     }
 
     public Book getBookById(Integer id) {
-        return bookDao.getBookById(id);
+        return bookMapper.getBookById(id);
     }
 
     public List<Book> getAllBooks() {
-        return bookDao.getAllBooks();
+        return bookMapper.getAllBooks();
     }
 }
