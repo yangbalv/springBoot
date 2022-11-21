364c68fb302956918d61c36d3386d0f3e7e03def
张天一 <1539626240@qq.com>
2022-11-21 15:14:13 +0800

    SpringBoot-RestFul服务，@RepositoryRestResource 可以对restFul服务进行配置，包括路径，此类是否进行配置，以及rest响应名称等等， 继承JpaRepository类的接口可以添加方法，默认可以通过方法名称来访问方法，@RestResource配置添加的方法，配置请求名称和rest中的名称。

--- src/main/java/com/springboot/live_comm/dao/JPABookDao.java
+++ src/main/java/com/springboot/live_comm/dao/JPABookDao.java
@@ -5,12 +5,19 @@ import com.springboot.live_comm.entity.JPABook;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
+//restful的请求路径设计，path设置为aaaa exported表示不会被restFul规则访问
+@RepositoryRestResource(exported = false, path = "aaaaaa", collectionResourceRel = "bb", itemResourceRel = "lll")
 //Spring Data JPA中只要方法的定义符合既定规范，Spring Data 就可以分析出开发者的意图，完成特定的SQL进而完成Sql的设计
 public interface JPABookDao extends JpaRepository<JPABook, Integer> {
-    List<JPABook> getJPABooksByAuthorStartingWith(String author);
+    //    继承了JpaRepository后在类中写的方法按照方法名称来进行访问
+//    RestResource来重新设置方法的请求设置 @Param设置请求参数的名称
+    @RestResource(path = "aa", rel = "bb")
+    List<JPABook> getJPABooksByAuthorStartingWith(@Param("cc") String author);
 
     List<JPABook> getJPABooksByPriceGreaterThan(Float price);
 
