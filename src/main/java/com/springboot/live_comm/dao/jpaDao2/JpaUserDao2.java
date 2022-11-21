0ab426e8978b0383223b87d6407755607ddca33f
张天一 <1539626240@qq.com>
2022-11-21 15:50:53 +0800

    SpringBoot-RestFul服务-@RepositoryRestResource可以配置restful的访问路径，rest展示时的路径，get请求获取的json中的连接的名称,多数据源配置时配置在生效的继承类即可,继承类中可以添加方法，利用@RestResource进行方法的配置，自定义的方法的请求都需要添加在search之后，默认以方法名称作为请求地址，此处写的方法不能进行重载。

--- src/main/java/com/springboot/live_comm/dao/jpaDao2/JpaUserDao2.java
+++ src/main/java/com/springboot/live_comm/dao/jpaDao2/JpaUserDao2.java
@@ -2,7 +2,10 @@ package com.springboot.live_comm.dao.jpaDao2;
 
 import com.springboot.live_comm.entity.JPAUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RepositoryRestResource(path = "zzzzz", collectionResourceRel = "bs" ,itemResourceRel = "b" ,exported = false)
 public interface JpaUserDao2 extends JpaRepository<JPAUser, Integer> {
 
 }
