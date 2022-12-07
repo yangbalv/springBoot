7b0ecb325fd145d49afd85034339720789c98e72
张天一 <1539626240@qq.com>
2022-12-07 17:44:46 +0800

    springBoot-安全管理-spring Security-springSecurity的配置：创建配置类继承WebSecurityConfigurerAdapter，添加passwordEncoder bean设置密码加密方式， configure重写configure（AuthenticationManagerBuilder为请求参数参数）方法添加用户并设置密码以及权限。重写configure(HttpSecurity为请求参数)方法设置HttpSecurity，http.authorizeRequests()表示开启HttpSecurity配置，antMatchers，hasRole组合使用为指定的接口设置指定的访问权限，formLogin表示开启表单登录,springSecurity默认会重制/login的get和post请求，其中的get请求返回授权页面，post请求接受form-data的表单数据用作授权登录接口， 可以使用.loginPage修改授权页面则会更具loginPage配置的值跳转到对应的接口，配置成/login也是跳转到/login接口，loginProcessingUrl配置授权信息提交接口，配置之后授权则是需要向对应的接口发送from-data数据。通过successHandler设置授权成功的返回信息格式等。.logout设置注销操作logout会重制/logout的service接口，logout可以设置清除哪些数据。

--- src/main/java/com/springboot/live_comm/controller/security/SecurityHelloController.java
+++ src/main/java/com/springboot/live_comm/controller/security/SecurityHelloController.java
@@ -1,12 +1,40 @@
 package com.springboot.live_comm.controller.security;
 
+import com.springboot.live_comm.model.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class SecurityHelloController {
+    @GetMapping(value = "/security/login")
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
     @GetMapping("/securityHello")
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/admin/securityHello")
+    public String admin() {
+        return "hello admin";
+    }
+
+    @GetMapping("/user/securityHello")
+    public String user() {
+        return "hello";
+    }
+
+    @GetMapping("/db/securityHello")
+    public String dba() {
+        return "hello dba";
+    }
+
 }
