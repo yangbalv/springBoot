164135a4a594e3a643978a82e973fbe2e6552b2d
张天一 <1539626240@qq.com>
2022-12-12 23:08:52 +0800

    springBoot-安全管理-springSecurity-*****整合数据库******，数据库表（创建在chapter05-1表）

--- 数据库文件/mysql_chapter05.sql
+++ 数据库文件/mysql_chapter05.sql
@@ -46,14 +46,84 @@ INSERT INTO `book` (`id`, `name`, `author`)
 VALUES (1, '水浒传', '施耐庵');
 
 
-
 -- JPA多数据源操作时不自动生成表，需要自主创建表
-SET FOREIGN_KEY_CHECKS=0;
+SET
+FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user` (
-                          `id` int(11) NOT NULL,
-                          `name` varchar(255) DEFAULT NULL,
-                          `gender` varchar(255) DEFAULT NULL,
-                          `age` int(11) DEFAULT NULL,
-                          PRIMARY KEY (`id`)
+CREATE TABLE `t_user`
+(
+    `id`     int(11) NOT NULL,
+    `name`   varchar(255) DEFAULT NULL,
+    `gender` varchar(255) DEFAULT NULL,
+    `age`    int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- springSecurity 用户权限表（user）
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`
+(
+    `id`       int(11) NOT NULL,
+    `username` varchar(32)  NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `enabled`  tinyint(1) NOT NULL,
+    `locked`   tinyint(1) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user`
+VALUES ('1', 'zty', '$2a$10$Pa8lMflBy0v2RPqLzc6haOPXj/WbLsCtJKwSc3KNkj5a7NmW9YUre', '1', '0');
+
+-- springSecurity 用户权限表（role）
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`
+(
+    `id`     int(11) NOT NULL,
+    `name`   varchar(32) NOT NULL,
+    `nameZh` varchar(32) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role`
+VALUES ('1', 'ROLE_ADMIN', '系统管理员');
+INSERT INTO `role`
+VALUES ('2', 'ROLE_DBA', '数据库管理员');
+INSERT INTO `role`
+VALUES ('3', 'ROLE_USER', '用户');
+
+
+-- springSecurity 用户权限表（user_role）
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`
+(
+    `id`  int(11) NOT NULL,
+    `uid` int(11) NOT NULL,
+    `rid` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role`
+VALUES ('1', '1', '1');
+INSERT INTO `user_role`
+VALUES ('2', '1', '2');
+INSERT INTO `user_role`
+VALUES ('3', '1', '3');
