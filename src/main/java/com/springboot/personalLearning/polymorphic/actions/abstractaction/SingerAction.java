e7e045bc5e4215c718faf4b088ab5bf7340a86d1
张天一 <zhangtianyi@everelegance.com.cn>
2023-01-30 17:59:21 +0800

    Revert "预创建自定义式的websocket的设置类"
    This reverts commit 82fd4a1e

--- src/main/java/com/springboot/personalLearning/polymorphic/actions/abstractaction/SingerAction.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.personalLearning.polymorphic.actions.abstractaction;
-
-import com.springboot.personalLearning.polymorphic.actions.action.PersonAction;
-
-public interface SingerAction extends PersonAction {
-    void sing(String song);
-}
--- src/main/java/com/springboot/personalLearning/polymorphic/models/model/Person.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.personalLearning.polymorphic.models.model;
-
-public class Person {
-    public String name;
-    public int age;
-
-//    抽象方法必须放在抽象类中
-//public abstract void work();
-}
