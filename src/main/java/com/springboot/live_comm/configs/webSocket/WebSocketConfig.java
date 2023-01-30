40a9f2ec0edbb0b2ac4a33737d62bee3e3c8f6fa
张天一 <zhangtianyi@everelegance.com.cn>
2023-01-30 17:52:13 +0800

    预创建自定义式的websocket的类型

--- src/main/java/com/springboot/live_comm/configs/webSocket/WebSocketConfig.java
+++ src/main/java/com/springboot/live_comm/configs/webSocket/WebSocketConfig.java
@@ -1,14 +1,30 @@
 package com.springboot.live_comm.configs.webSocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
+
+/**
+ * 说明：WebScoket配置处理器
+ * 把处理器和拦截器注册到spring websocket中
+ *
+ * @author 张天一
+ * @version 1.0
+ * @date 23年01月19号09点44分
+ */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSocketConfigurer {
+
+
+    //注入处理器
+    @Autowired
+    private ChatWebSocketHandler webSocketHandler;
+    @Autowired
+    private ChatHandshakeInterceptor chatHandshakeInterceptor;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
 //添加一个群发与点对点消息发送的功能
@@ -20,5 +36,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat").withSockJS();
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        首先添加了一个ws的websocket的接口
+//        页面请求 / ws时数据发送至websocket
+        registry.addHandler(webSocketHandler, "/ws").addInterceptors(chatHandshakeInterceptor);
+
+
+//        //添加一个处理器还有定义处理器的处理路径
+//        registry.addHandler(webSocketHandler, "/ws").addInterceptors(chatHandshakeInterceptor);
+//        /*
+//         * 在这里我们用到.withSockJS()，SockJS是spring用来处理浏览器对websocket的兼容性，
+//         * 目前浏览器支持websocket还不是很好，特别是IE11以下.
+//         * SockJS能根据浏览器能否支持websocket来提供三种方式用于websocket请求，
+//         * 三种方式分别是 WebSocket, HTTP Streaming以及 HTTP Long Polling
+//         */
+//        registry.addHandler(webSocketHandler, "/ws/sockjs").addInterceptors(chatHandshakeInterceptor).withSockJS();
     }
 }
