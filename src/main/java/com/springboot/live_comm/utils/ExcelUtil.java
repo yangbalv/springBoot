f30cb6d6ac4bf56b4bfda54294fd111cbae80661
张天一 <zhangtianyi@everelegance.com.cn>
2023-02-15 11:37:37 +0800

    EXCEL读取工具类。

---
+++ src/main/java/com/springboot/live_comm/utils/ExcelUtil.java
@@ -0,0 +1,73 @@
+package com.springboot.live_comm.utils;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ExcelUtil {
+    public static void main(String[] args) {
+        String url = "D:\\ZTY\\aa.xlsx";
+        ExcelUtil.doExecute(url);
+    }
+
+    public static void doExecute(String url) {
+        //excel文件路径
+        String excelPath = url;
+
+        try {
+            //String encoding = "GBK";
+            File excel = new File(excelPath);
+            if (excel.isFile() && excel.exists()) {   //判断文件是否存在
+
+                String[] split = excel.getName().split("\\.");  //.是特殊字符，需要转义！！！！！
+                Workbook wb;
+                //根据文件后缀（xls/xlsx）进行判断
+                if ("xls".equals(split[1])) {
+                    FileInputStream fis = new FileInputStream(excel);   //文件流对象
+                    wb = new HSSFWorkbook(fis);
+                } else if ("xlsx".equals(split[1])) {
+                    wb = new XSSFWorkbook(excel);
+                } else {
+                    System.out.println("文件类型错误!");
+                    return;
+                }
+
+                //开始解析
+                Sheet sheet = wb.getSheetAt(0);     //读取sheet 0
+
+                int firstRowIndex = sheet.getFirstRowNum() + 1;   //第一行是列名，所以不读
+                int lastRowIndex = sheet.getLastRowNum();
+                System.out.println("firstRowIndex: " + firstRowIndex);
+                System.out.println("lastRowIndex: " + lastRowIndex);
+
+                for (int rIndex = firstRowIndex; rIndex <= lastRowIndex; rIndex++) {   //遍历行
+                    System.out.println("rIndex: " + rIndex);
+                    Row row = sheet.getRow(rIndex);
+                    if (row != null) {
+                        int firstCellIndex = row.getFirstCellNum();
+                        int lastCellIndex = row.getLastCellNum();
+                        for (int cIndex = firstCellIndex; cIndex < lastCellIndex; cIndex++) {   //遍历列
+                            Cell cell = row.getCell(cIndex);
+                            if (cell != null) {
+                                System.out.println(cell.toString());
+                            }
+                        }
+                    }
+                }
+            } else {
+                System.out.println("找不到指定的文件");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
