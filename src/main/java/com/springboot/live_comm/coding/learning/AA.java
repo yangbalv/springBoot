7d8aee9d3c890f2c3f83f1eda01753a116a60bff
张天一 <zhangtianyi@everelegance.com.cn>
2023-11-09 17:47:55 +0800

    利用map和set实现的并查集

--- src/main/java/com/springboot/live_comm/coding/learning/AA.java
+++ src/main/java/com/springboot/live_comm/coding/learning/AA.java
@@ -34,12 +34,88 @@ public class AA {
     public Map<NodeX, NodeX> map = new HashMap<>();
 
     public NodeX cloneGraph(NodeX node) {
-        if (map.containsKey(node)){
+        NodeX newNode;
+        if (!map.containsKey(node)) {
+            newNode = new NodeX(node.val);
+            List<NodeX> nodeXList = new ArrayList<>();
+            map.put(node, newNode);
+            for (NodeX neighbor : node.neighbors) {
+                nodeXList.add(cloneGraph(neighbor));
+            }
+            newNode.neighbors = nodeXList;
+        } else {
+            newNode = map.get(node);
+        }
+        return newNode;
+    }
+
+    //    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+//        if (equations==null||equations.size()!= values.length){
+//            return new double[]{};
+//        }
+//        for (List<String> equation : equations) {
+//            String s1 = equation.get(0);
+//            String s2 = equation.get(1);
+//        }
+//
+//    }
+    public static void main(String[] args) {
+        AA aa = new AA();
+        System.out.println(aa.equationsPossible(new String[]{"a==b", "b==c","c==d", "d==a"}));
+    }
+
+    public boolean equationsPossible(String[] equations) {
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Integer, Set<Character>> setMap = new HashMap<>();
+        Map<Integer, Set<Integer>> upsetDown = new HashMap<>();
+        int p = 1;
+        for (int i = 0; i < equations.length; i++) {
+            char c1 = equations[i].charAt(0);
+            char c2 = equations[i].charAt(3);
+            p = getP(map, setMap, upsetDown, p, c1);
+            p = getP(map, setMap, upsetDown, p, c2);
+            Integer c1Num = map.get(c1);
+            Integer c2Num = map.get(c2);
+            Set<Character> c1Set = setMap.get(c1Num);
+            Set<Character> c2Set = setMap.get(c2Num);
+            Set<Integer> upsetDownSet1 = upsetDown.get(-c1Num);
+            Set<Integer> upsetDownSet2 = upsetDown.get(-c2Num);
+
+            if (equations[i].charAt(1) == '=') {
+                if (!c1Num.equals(c2Num)) {
+                    if (upsetDown.get(-c1Num).contains(c2Num) || upsetDown.get(-c2Num).contains(c1Num)) {
+                        return false;
+                    }else {
+                        c1Set.addAll(c2Set);
+                        upsetDownSet1.addAll(upsetDownSet2);
+                        for (Character character : c2Set) {
+                            map.put(character,c1Num);
+                        }
+                    }
+                }
+            } else {
+                upsetDown.get(-c1Num).add(c2Num);
+                upsetDown.get(-c2Num).add(c1Num);
+                if (map.get(c2).equals(map.get(c1))) {
+                    return false;
+                }
+            }
 
         }
-        NodeX new
-        stack
+        return true;
+    }
 
+    private int getP(Map<Character, Integer> map, Map<Integer, Set<Character>> setMap, Map<Integer, Set<Integer>> upsetDown, int p, char c2) {
+        if (!map.containsKey(c2)) {
+            Set<Character> set1 = new HashSet<>();
+            Set<Integer> set2 = new HashSet<>();
+            set1.add(c2);
+            map.put(c2, p);
+            setMap.put(p, set1);
+            upsetDown.put(-p, set2);
+            p++;
+        }
+        return p;
     }
 
 }
