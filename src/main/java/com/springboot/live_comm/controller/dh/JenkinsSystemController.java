1d18e36ebc75d3b019bc0ef693b0bae5f830fcad
张天一 <zhangtianyi@everelegance.com.cn>
2024-01-09 19:48:42 +0800

    东华项目

--- src/main/java/com/springboot/live_comm/controller/dh/JenkinsSystemController.java
+++ src/main/java/com/springboot/live_comm/controller/dh/JenkinsSystemController.java
@@ -14,22 +14,24 @@ import java.io.IOException;
 @Slf4j
 public class JenkinsSystemController {
 
-    @GetMapping("/{system}")
-    public void jenkins(@PathVariable String system) throws IOException {
-        String systemValue = SystemEnums.valueOf(system).getKey();
-        String path = "http://183.195.144.230:9503/dhjk/view/dhbailian-publish/job/" + systemValue +
-                "/buildWithParameters?token=qwertyui&branch=feat/dh2.2_dependency_cleanup&deploy_env=dh2dev";
-        HttpUtil httpUtil = new HttpUtil();
-        httpUtil.getHTML(path);
-    }
+    @GetMapping("/dh/{system}")
+    public String jenkins(@PathVariable String system) throws IOException {
+        if ("publishAll".equals(system)){
+            for (SystemEnums value : SystemEnums.values()) {
+                String path = "http://183.195.144.230:9503/dhjk/view/dhbailian-publish/job/" + value.getKey() +
+                        "/buildWithParameters?token=qwertyui&branch=feat/dh2.2_dependency_cleanup&deploy_env=dh2dev";
+                HttpUtil httpUtil = new HttpUtil();
+                httpUtil.getHTML(path);
+            }
+            return "调用jenkins发布全部应用未出现异常，详情去 （http://183.195.144.230:9503/dhjk/） 页面查看";
+        }else {
 
-    @GetMapping("/all")
-    public void jenkinsAll() throws IOException {
-        for (SystemEnums value : SystemEnums.values()) {
-            String path = "http://183.195.144.230:9503/dhjk/view/dhbailian-publish/job/" + value.getKey() +
+            String systemValue = SystemEnums.valueOf(system).getKey();
+            String path = "http://183.195.144.230:9503/dhjk/view/dhbailian-publish/job/" + systemValue +
                     "/buildWithParameters?token=qwertyui&branch=feat/dh2.2_dependency_cleanup&deploy_env=dh2dev";
             HttpUtil httpUtil = new HttpUtil();
             httpUtil.getHTML(path);
+            return "调用jenkins发布"+system+"应用未出现异常，详情去 （http://183.195.144.230:9503/dhjk/） 页面查看";
         }
     }
 }
