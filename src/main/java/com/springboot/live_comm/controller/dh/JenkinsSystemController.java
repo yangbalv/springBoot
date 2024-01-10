1cce157fc83ee5a0d2daafecb75e460336473e6f
张天一 <zhangtianyi@everelegance.com.cn>
2024-01-10 16:52:35 +0800

    东华项目

--- src/main/java/com/springboot/live_comm/controller/dh/JenkinsSystemController.java
+++ src/main/java/com/springboot/live_comm/controller/dh/JenkinsSystemController.java
@@ -23,23 +23,23 @@ public class JenkinsSystemController {
                 HttpUtil httpUtil = new HttpUtil();
                 System.out.println(httpUtil.getHTML(path));
             }
-            return "调用jenkins发布全部应用未出现异常，详情去 （http://183.195.144.230:9503/dhjk/） 页面查看";
+            return "调用jenkins发布全部应用成功，详情去 （http://183.195.144.230:9503/dhjk/） 页面查看";
         } else {
             String systemValue = SystemEnums.valueOf(system).getPublish();
             String path = "http://183.195.144.230:9503/dhjk" + systemValue +
                     "/buildWithParameters?token=qwertyui&branch=feat/dh2.2_dependency_cleanup&deploy_env=dh2dev";
             HttpUtil httpUtil = new HttpUtil();
             System.out.println(httpUtil.getHTML(path));
-            return "发布" + system + "应用未出现异常，详情在 （http://183.195.144.230:9503/dhjk/） 页面查看";
+            return "发布" + system + "应用成功，详情在 （http://183.195.144.230:9503/dhjk/） 页面查看";
         }
     }
-    @GetMapping("/ddl/{system}")
+    @GetMapping("/de/{system}")
     public String deJenkins(@PathVariable String system) throws IOException {
         String systemValue = SystemEnums.valueOf(system).getDeploy();
         String path = "http://183.195.144.230:9503/dhjk" + systemValue +
                 "/buildWithParameters?token=qwertyui&branch=feat/dh2.2_dependency_cleanup&deploy_env=dh2dev";
         HttpUtil httpUtil = new HttpUtil();
         System.out.println(httpUtil.getHTML(path));
-        return "deploy" + system + "应用未出现异常，详情去 （http://183.195.144.230:9503/dhjk/） 页面查看";
+        return "deploy" + system + "应用成功，详情去 （http://183.195.144.230:9503/dhjk/） 页面查看";
     }
 }
